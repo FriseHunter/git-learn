6187a605819edfe74ef69d5b2b05325662b22d72
Test User <freesoul2708@gmail.com>
2018-09-06 14:59:51 +0300

    test scripts

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 'use strict';
+alert('I working')
+
+const input = document.querySelectorAll('input');
+
+input.addEvwentListener('change' (e) => {
+	let p = createElement(p);
+	let text = window.body.appendChild('p')
+	text.innerHTML = e.target.value
+});
