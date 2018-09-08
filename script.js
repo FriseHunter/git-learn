126e13b50d055127bba78cf4128810e87d46cfd7
Test User <freesoul2708@gmail.com>
2018-09-08 13:27:24 +0300

    modefied index & scripts

--- script.js
+++ script.js
@@ -1,10 +1,2 @@
-'use strict';
+
 alert('I working')
-
-const input = document.querySelectorAll('input');
-
-input.addEvwentListener('change' (e) => {
-	let p = createElement(p);
-	let text = window.body.appendChild('p')
-	text.innerHTML = e.target.value
-});
